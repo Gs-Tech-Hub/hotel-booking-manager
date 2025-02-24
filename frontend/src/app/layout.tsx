91a30cfe3e2d143e7f2ecab784094c3af7121f02
slowdev420 <abc.zeezay@gmail.com>
2025-02-24 11:03:13 +0100

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
