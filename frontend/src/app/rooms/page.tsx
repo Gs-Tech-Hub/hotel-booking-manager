655672698f5d03bcdb471f0b500b79b4d189847d
Oyeins-GUI <oyeinx7@gmail.com>
2025-03-13 12:51:15 +0100

    created a page for a single room

--- frontend/src/app/rooms/page.tsx
+++ frontend/src/app/rooms/page.tsx
@@ -1,12 +1,5 @@
-import Image from 'next/image';
+import RoomSection from "@/components/room-section";
 
 export default function Rooms() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold">Rooms</h1>
-        <p className="mt-2">Explore our available rooms.</p>
-        <Image src="/images/rooms.jpg" alt="Rooms" width={500} height={300} />
-      </div>
-    );
-  }
-  
+   return <RoomSection />;
+}
