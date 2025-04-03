6b36f3d056b6752ead2e62b65ea997c4d3cd2d81
Godsent Clement <36302811+gsaint12@users.noreply.github.com>
2025-04-03 16:20:05 +0100

    chatwoot chat

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,20 +32,24 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Script src="/js/bootstrap.bundle.min.js"  />
         <Script src="/js/custom-script.js"  />
         <Script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js" />
-        <Script id="tawk-chat-widget" type="text/javascript">
+        <Script id="chatwoot-widget" strategy="lazyOnload">
           {`
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/67e9f5fee63eda190bc460af/1inkuhrd0';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
+          (function(d,t) {
+            var BASE_URL="https://app.chatwoot.com";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.defer = true;
+            g.async = true;
+            s.parentNode.insertBefore(g,s);
+            g.onload=function(){
+              window.chatwootSDK.run({
+                websiteToken: '${process.env.CHATWOOT_TOKEN}',
+                baseUrl: BASE_URL
+              })
+            }
+          })(document,"script");
           `}
         </Script>
-
 
         {/* Header */}
         <header>
