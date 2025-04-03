b29df6e0d5e41f598884ad1390a82488446a39af
Godsent Clement <36302811+gsaint12@users.noreply.github.com>
2025-04-03 08:24:11 +0100

    ii.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -30,18 +30,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Script src="/js/bootstrap.bundle.min.js"  />
         <Script src="/js/custom-script.js"  />
         <Script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js" />
-        
-            <Script type="text/javascript">
-            {`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/67e9f5fee63eda190bc460af/1inkuhrd0';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();`}
-            </Script>
         {/* Header */}
         <header>
           <Navigation />
