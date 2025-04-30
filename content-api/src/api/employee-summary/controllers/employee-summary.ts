679a3516972a04e68a96c1f1c06d31108cb1fa58
Godsent Clement <gsnigeriatech@gmail.com>
2025-04-30 14:55:38 +0100

    employee summary: setup

---
+++ content-api/src/api/employee-summary/controllers/employee-summary.ts
@@ -0,0 +1,7 @@
+/**
+ * employee-summary controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::employee-summary.employee-summary');
---
+++ content-api/src/api/employee-summary/services/employee-summary.ts
@@ -0,0 +1,7 @@
+/**
+ * employee-summary service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::employee-summary.employee-summary');
