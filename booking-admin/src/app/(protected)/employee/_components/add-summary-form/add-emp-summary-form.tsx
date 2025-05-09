36c0be5c9b8b8203a0cbe72d70acc79460b72b8a
Godsent Clement <gsnigeriatech@gmail.com>
2025-05-09 20:35:06 +0100

    add: employee recs

--- booking-admin/src/app/(protected)/employee/_components/add-summary-form/add-emp-summary-form.tsx
+++ booking-admin/src/app/(protected)/employee/_components/add-summary-form/add-emp-summary-form.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable */
+import { Button } from '@/components/ui-elements/button';
 import { strapiService } from '@/utils/dataEndPoint';
 import React, { useState } from 'react';
 import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 interface AddEmployeeSummaryFormProps {
   id?: number;
   documentId: string;
+  employmentDate: Date;
+  salary: number;
   order_discount_total?: number;
   debt_shortage?: number;
   fines_debits?: number;
@@ -20,12 +22,15 @@ interface AddEmployeeSummaryFormProps {
 const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProps, onSubmit }: { initialData?: AddEmployeeSummaryFormProps, onSubmit: (data: AddEmployeeSummaryFormProps) => void }) => {
   const [form, setForm] = useState({
     id: initialData.id,
-    order_discount_total: initialData.order_discount_total || 0,
-    debt_shortage: initialData.debt_shortage || 0,
-    fines_debits: initialData.fines_debits || 0,
-    salary_advanced: initialData.salary_advanced || 0,
-    salary_advanced_status: initialData.salary_advanced_status || 'pending',
-    user_documentId: initialData.users_permissions_user?.documentId || '',
+    documentId: initialData.documentId,
+    order_discount_total: initialData.order_discount_total,
+    employmentDate: initialData.employmentDate,
+    salary: initialData.salary,
+    debt_shortage: initialData.debt_shortage,
+    fines_debits: initialData.fines_debits,
+    salary_advanced: initialData.salary_advanced,
+    salary_advanced_status: initialData.salary_advanced_status,
+    user_documentId: initialData.users_permissions_user?.documentId,
   });
   console.log('initialData:', initialData);
 
@@ -40,7 +45,8 @@ const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProp
     try {
       // Create a payload object excluding non-form fields
       const payload = {
-        users_permissions_user: form.id,
+        employmentDate: form.employmentDate,
+        salary: form.salary,
         order_discount_total: form.order_discount_total,
         debt_shortage: form.debt_shortage,
         fines_debits: form.fines_debits,
@@ -52,7 +58,7 @@ const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProp
         throw new Error("Employee ID is required for updating");
       }
 
-      const res = await strapiService.createEmployeeSummary(payload);
+      const res = await strapiService.updateEmployeeSummary(form.documentId, payload);
       toast.success("Employee summary updated successfully!");
       onSubmit(res);
     } catch (error: any) {
@@ -62,8 +68,27 @@ const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProp
 
   return (
     <form onSubmit={handleSubmit} className="max-w-xl mx-auto p-4 bg-white shadow rounded space-y-4">
-      <h2 className="text-xl font-semibold">Employee Management Form</h2>
-      <div>
+       <div>
+        <label className="block mb-1 font-medium">Employment Date</label>
+        <input
+          type="date"
+          name="employmentDate"
+          value={form.employmentDate ? new Date(form.employmentDate).toISOString().slice(0, 10) : ''}
+          onChange={handleChange}
+          className="w-full border border-gray-300 p-2 rounded"
+        />
+      </div>
+       <div>
+        <label className="block mb-1 font-medium">Salary</label>
+        <input
+          type="number"
+          name="salary"
+          value={form.salary}
+          onChange={handleChange}
+          className="w-full border border-gray-300 p-2 rounded"
+        />
+      </div>
+      {/* <div>
         <label className="block mb-1 font-medium">Order Discount Total</label>
         <input
           type="number"
@@ -72,7 +97,7 @@ const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProp
           onChange={handleChange}
           className="w-full border border-gray-300 p-2 rounded"
         />
-      </div>
+      </div> */}
 
       <div>
         <label className="block mb-1 font-medium">Debt Shortage</label>
@@ -121,12 +146,11 @@ const AddEmployeeSummaryForm = ({ initialData = {} as AddEmployeeSummaryFormProp
         </select>
       </div>
 
-      <button
-        type="submit"
+      <Button
+        label={initialData.id ? 'Update' : 'Create'}
         className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-      >
-        {initialData.id ? 'Update' : 'Create'}
-      </button>
+
+      />
     </form>
   );
 };
